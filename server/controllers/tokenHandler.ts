385289df55847317b4e0fe9b4bb647090c60b365
Eirik Ronæss Hansen <eirikrhansen@gmail.com>
2024-04-23 20:10:33 +0200

    Addn interface for token data

--- server/controllers/tokenHandler.ts
+++ server/controllers/tokenHandler.ts
@@ -2,7 +2,12 @@ const jwt = require('jsonwebtoken');
 
 export interface TokenHandler {
   createToken(tokenData): string;
-  decodeToken(token: string): string;
+  decodeToken(token: string): TokenData;
+}
+
+interface TokenData {
+  username: string;
+  userID: number
 }
 
 export class jwtTokenHandler implements TokenHandler {
@@ -14,7 +19,7 @@ export class jwtTokenHandler implements TokenHandler {
     return jwt.sign(tokenData, this.encoderKey);
   }
 
-  public decodeToken(token: string): string {
+  public decodeToken(token: string): TokenData {
     return jwt.verify(token, this.encoderKey);
   }
 }
