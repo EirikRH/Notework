7b74da73c4436d55a3441b76dc6ae8556a75fdb4
Eirik Ronæss Hansen <eirikrhansen@gmail.com>
2024-04-23 20:34:35 +0200

    renaming for cleanliness

--- server/server.ts
+++ server/server.ts
@@ -3,22 +3,22 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const port = 3001
+const port = 3001;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-import { JwtTokenHandler, TokenHandler } from './controllers/tokenHandler';
-import { DatabaseUserAuthenticator, UserAuthenticator } from './controllers/userAuthenticator';
-import { DatabaseUserHandler, UserController } from './controllers/databaseUserHandler';
+import { JwtTokenHandler, TokenHandler } from './userControllers/tokenHandler';
+import { DatabaseUserAuthenticator, UserAuthenticator } from './userControllers/userAuthenticator';
+import { DatabaseUserHandler, UserController } from './userControllers/databaseUserHandler';
 
 const tokenHandler: TokenHandler = new JwtTokenHandler(process.env.SECRET_KEY!);
 const userAuthenticator: UserAuthenticator = new DatabaseUserAuthenticator();
 const userHandler: UserController = new DatabaseUserHandler();
 
 app.post('/createUser', async (req, res) => {
-  const { username, password } = req.body.newUserData;
+  const { username, password } = req.body;
   try {
     await userHandler.createUser(username, password);
     res.status(201).send('User created successfully');
@@ -28,32 +28,30 @@ app.post('/createUser', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-  const { username, password } = req.body.credentials;
+  const { username, password } = req.body;
 
   try {
-    const login = await userAuthenticator.authenticateLoginAttempt(username, password)
-    const token = tokenHandler.createToken(login);
+    const login = await userAuthenticator.authenticateLoginAttempt(username, password);
+    const loginToken = tokenHandler.createToken(login);
 
-    res.status(200).json({ token });
-  } catch (error) {
-    
-  }
+    res.status(200).json({ loginToken });
+  } catch (error) {}
 });
 
 app.post('/newNote', async (req, res) => {
-  const { title, content} = req.body.newNote;
-  const loginToken = req.body.loginToken;
-})
-
-app.get('/userNotes', async (req, res) => {
-  const loginToken = req.body.loginToken;
-})
-
-app.put('/updateNote', async (req, res) => {
-  const { noteID, newTitle, newContent } = req.body.updatedNote;
+  const { title, content } = req.body;
   const loginToken = req.body.loginToken;
 });
 
-app.listen({port}, () => {
+app.get('/userNotes', async (req, res) => {
+  const loginToken = req.body.loginToken;
+});
+
+app.put('/updateNote', async (req, res) => {
+  const { noteID, newTitle, newContent } = req.body;
+  const loginToken = req.body.loginToken;
+});
+
+app.listen({ port }, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
